c7ee54c100d779c02ecb17b437cb5a3571c30262
Lê Sĩ Bích <le.si.bich@sun-asterisk.com>
2019-03-23 21:15:45 +0700

    Merge Dot net (#7)

---
+++ dot_net/Spelling/Spelling/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spelling
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			SpellChecker sc = new SpellChecker();
+			Console.WriteLine(sc.Check("nghengh"));
+		}
+	}
+}
