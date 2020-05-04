543eb64db711bcd57716b3a22f0c7001e762e4cf
_MoonLight_ <moonlight8978@gmail.com>
2020-05-04 21:30:14 +0700

    custom react navigation stack header

---
+++ App.js
@@ -0,0 +1,139 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  createStackNavigator,
+  HeaderHeightContext,
+} from "@react-navigation/stack";
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "green",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  headerButton: {
+    width: 100,
+  },
+  button: {
+    flex: 1,
+  },
+  text: {
+    flex: 1,
+    alignItems: "center",
+  },
+});
+
+function ScreenHeader({ title, leftButton, rightButton, style }) {
+  return (
+    <View style={[style, styles.header]}>
+      <View style={styles.headerButton}>{leftButton}</View>
+      <View style={styles.text}>
+        <Text>{title}</Text>
+      </View>
+      <View style={styles.headerButton}>{rightButton}</View>
+    </View>
+  );
+}
+
+function ScreenHeaderButton({ onPress }) {
+  return <Button title="Sample" onPress={onPress} style={styles.button} />;
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button title="Go" onPress={() => navigation.navigate("Inner")} />
+      <Button title="Back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={({ navigation }) => ({
+          headerTitle: ({ children }) => (
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text>{children}</Text>
+            </View>
+          ),
+          headerBackTitleVisible: false,
+          headerLeft: ({ canGoBack, ...rest }) => {
+            console.log(rest);
+            return canGoBack ? (
+              <Button onPress={() => navigation.goBack()} title="Back" />
+            ) : undefined;
+          },
+          headerBackground: () => (
+            <View
+              style={[{ backgroundColor: "red" }, StyleSheet.absoluteFill]}
+            />
+          ),
+        })}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Home" }}
+        />
+        <Stack.Screen
+          name="Inner"
+          component={HomeScreen}
+          options={{ title: "Inner" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+// header: ({ scene, previous, navigation }) => {
+//   const { options } = scene.descriptor;
+//   const title =
+//     options.headerTitle !== undefined
+//       ? options.headerTitle
+//       : options.title !== undefined
+//       ? options.title
+//       : scene.route.name;
+//   return (
+//     <ScreenHeader
+//       title={title}
+//       leftButton={
+//         previous ? (
+//           <ScreenHeaderButton onPress={navigation.goBack} />
+//         ) : undefined
+//       }
+//       style={options.headerStyle}
+//     />
+//   );
+// },
+// header: ({ scene, previous, navigation }) => {
+//   const { options } = scene.descriptor;
+//   const title =
+//     options.headerTitle !== undefined
+//       ? options.headerTitle
+//       : options.title !== undefined
+//       ? options.title
+//       : scene.route.name;
+//   return (
+//     <View style={options.headerStyle}>
+//       {previous ? <Text>Back</Text> : undefined}
+//       <Text>{title}</Text>
+//     </View>
+//   );
+// },
+// headerMode: "screen",
+// headerStyle: {
+//   height: 64,
+//   // paddingTop: 20,
+// },
